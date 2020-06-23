27b8c365a10584ced5d76e6fa2913b3600b5e9e1
Sundeep <sundeep@sundeeps-mbp.home>
2020-06-23 16:09:10 -0400

    added new files which create springboot weather api

---
+++ springboot-weather-api/src/main/java/com/spring/restapi/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.spring.restapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.spring.restapi")
+@SpringBootApplication
+public class DemoApplication { 
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ springboot-weather-api/src/main/java/com/spring/restapi/models/FormCityAttribute.java
@@ -0,0 +1,24 @@
+package com.spring.restapi.models;
+
+
+public class FormCityAttribute {
+	
+	private String city;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public FormCityAttribute(String city) {
+		super();
+		this.city = city;
+	}
+
+	public FormCityAttribute() {
+		super();
+	}
+}
